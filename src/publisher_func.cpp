5cfd507160f468fec7f72763b83b7a3d04c90e8b
Achuthankrishna <achkrish07@gmail.com>
2023-11-07 01:33:54 -0500

    Added Custom Publisher Message

--- src/publisher_func.cpp
+++ src/publisher_func.cpp
@@ -13,7 +13,7 @@
 void MinimalPublisher::timer_callback()
     {
       auto message = std_msgs::msg::String();
-      message.data = "Hello, world! " + std::to_string(count_++);
+      message.data = "Hello, I am ROS Humble, the LTS Version :p ! " + std::to_string(count_++);
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
