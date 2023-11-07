50364d4d2094cd8d17e528c64d0f66ec1fba6035
Achuthankrishna <achkrish07@gmail.com>
2023-11-07 01:32:51 -0500

    Hpp and Cpp files for publisher ready

---
+++ include/ROS2_Beginner_Tutorials/publisher_func.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file publisher_func.hpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#pragma once 
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+/**
+ * @brief Class for Publisher 
+ * 
+ */
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    }
+  private:
+  void timer_callback();
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    size_t count_;
+};
+
--- src/publisher_func.cpp
+++ src/publisher_func.cpp
@@ -8,40 +8,12 @@
  * @copyright Copyright (c) 2023
  * 
  */
-#include <chrono>
-#include <functional>
-#include <memory>
-#include <string>
+#include "../include/ROS2_Beginner_Tutorials/publisher_func.hpp"
 
-#include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-using namespace std::chrono_literals;
-
-/**
- * @brief Class for Publisher 
- * 
- */
-class MinimalPublisher : public rclcpp::Node
-{
-  public:
-    MinimalPublisher()
-    : Node("minimal_publisher"), count_(0)
-    {
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    }
-
-  private:
-    void timer_callback()
+void MinimalPublisher::timer_callback()
     {
       auto message = std_msgs::msg::String();
       message.data = "Hello, world! " + std::to_string(count_++);
       RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
       publisher_->publish(message);
     }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-};
