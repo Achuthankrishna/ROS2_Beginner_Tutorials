87326134894045afdeb4384fbd0704c0ba095f2e
Achuthankrishna <achkrish07@gmail.com>
2023-11-07 01:35:31 -0500

    Wrote main fuction for publisher

---
+++ src/p_main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file p_main.cpp
+ * @author Vyshnav Achuthan (vyachu07@umd.edu)
+ * @brief Main file for publisher call 
+ * @version 0.1
+ * @date 2023-11-07
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include "../include/ROS2_Beginner_Tutorials/publisher_func.hpp"
+int main(int argc, char * argv[]) {
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
