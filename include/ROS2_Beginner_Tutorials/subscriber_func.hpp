9e791a645e3c444ba2e343d1c9621d37a258290e
Achuthankrishna <achkrish07@gmail.com>
2023-11-21 04:14:29 -0500

    Cpp lint and Cpp checks

--- include/ROS2_Beginner_Tutorials/subscriber_func.hpp
+++ include/ROS2_Beginner_Tutorials/subscriber_func.hpp
@@ -1,34 +1,31 @@
 /**
  * @file subscriber_func.hpp
  * @author Vyshnav Achuthan (vyachu07@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-11-07
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #pragma once
 #include <memory>
-
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
 using std::placeholders::_1;
 using namespace std::chrono_literals;
 /**
  * @brief Subscriber Class
- * 
+ *
  */
-class MinimalSubscriber : public rclcpp::Node
-{
-  public:
-    MinimalSubscriber()
-    : Node("minimal_subscriber")
-    {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    }
-  private:
-      void topic_callback(const std_msgs::msg::String & msg) const;
-      rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+class MinimalSubscriber : public rclcpp::Node {
+ public:
+  MinimalSubscriber() : Node("minimal_subscriber") {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+        "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+  }
+
+ private:
+  void topic_callback(const std_msgs::msg::String& msg) const;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
 };
--- src/p_main.cpp
+++ src/p_main.cpp
@@ -1,17 +1,19 @@
 /**
  * @file p_main.cpp
  * @author Vyshnav Achuthan (vyachu07@umd.edu)
- * @brief Main file for publisher call 
+ * @brief Main file for publisher call
  * @version 0.1
  * @date 2023-11-07
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #include <iostream>
+
 #include "../include/ROS2_Beginner_Tutorials/publisher_func.hpp"
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
-  return 0;}
+  return 0;
+}
