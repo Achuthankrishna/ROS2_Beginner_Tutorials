62faca64908c3d5294e42027a1bdcb5db5c4b4cb
Achuthankrishna <achkrish07@gmail.com>
2023-11-21 04:14:29 -0500

    Cpp lint and Cpp checks

--- include/ROS2_Beginner_Tutorials/publisher_func.hpp
+++ include/ROS2_Beginner_Tutorials/publisher_func.hpp
@@ -1,45 +1,49 @@
 /**
  * @file publisher_func.hpp
  * @author Vyshnav Achuthan (vyachu07@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-11-07
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 #pragma once
 
 #include <chrono>
 #include <functional>
 #include <memory>
-#include <string>
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <string>
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "ros2_beginner_tutorials/srv/change_string.hpp"
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/static_transform_broadcaster.h"
-#include "geometry_msgs/msg/transform_stamped.hpp"
 
 using namespace std::chrono_literals;
 
-class MinimalPublisher : public rclcpp::Node
-{
-public:
-    MinimalPublisher();
-    // std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
+class MinimalPublisher : public rclcpp::Node {
+ public:
+  MinimalPublisher();
+  // std::shared_ptr<tf2_ros::StaticTransformBroadcaster>
+  // tf_static_broadcaster_;
 
-private:
-    void timer_callback();
-    void service_callback(const std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Request> request,
-                          std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Response> response);
+ private:
+  void timer_callback();
+  void service_callback(
+      const std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Request>
+          request,
+      std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Response>
+          response);
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    size_t count_;
-    std_msgs::msg::String message;
-    rclcpp::Service<ros2_beginner_tutorials::srv::ChangeString>::SharedPtr service_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  size_t count_;
+  std_msgs::msg::String message;
+  rclcpp::Service<ros2_beginner_tutorials::srv::ChangeString>::SharedPtr
+      service_;
 
-    void make_transforms();
-    std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
+  void make_transforms();
+  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
 };
--- src/publisher_func.cpp
+++ src/publisher_func.cpp
@@ -4,104 +4,105 @@
  * @brief Publisher in ROS2
  * @version 0.1
  * @date 2023-11-07
- * 
+ *
  * @copyright Copyright (c) 2023
- * 
+ *
  */
 
 #include "../include/ROS2_Beginner_Tutorials/publisher_func.hpp"
+
 #include "geometry_msgs/msg/transform_stamped.hpp"
 
 /**
  * @brief Construct a new Minimal Publisher:: Minimal Publisher object
- * 
+ *
  */
-MinimalPublisher::MinimalPublisher()
-    : Node("minimal_publisher"), count_(0) {
-//     std::string new_mesg_param = this->get_parameter("new_mesg").as_string();
-    this->declare_parameter<std::string>
-                        ("new_mesg",
-                        "Hello, I am ROS Humble, the LTS Version :p ! ");
-    std::string new_mesg_param = this->get_parameter("new_mesg").as_string();
+MinimalPublisher::MinimalPublisher() : Node("minimal_publisher"), count_(0) {
+  //     std::string new_mesg_param =
+  //     this->get_parameter("new_mesg").as_string();
+  this->declare_parameter<std::string>(
+      "new_mesg", "Hello, I am ROS Humble, the LTS Version :p ! ");
+  std::string new_mesg_param = this->get_parameter("new_mesg").as_string();
 
-    if (!new_mesg_param.empty()) {
-        message.data = new_mesg_param;
-        RCLCPP_ERROR_STREAM (this->get_logger(), "NO NEW MESSAGE");
-    } else {
-      message.data = "Hello, I am ROS Humble, the LTS Version :p ! ";}
-      // message.data = "Hello, I am ROS Humble, the LTS Version :p ! " +
-      // std::to_string(count_++);
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
-      this->make_transforms();
-    timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::
-                                    timer_callback, this));
+  if (!new_mesg_param.empty()) {
+    message.data = new_mesg_param;
+    RCLCPP_ERROR_STREAM(this->get_logger(), "NO NEW MESSAGE");
+  } else {
+    message.data = "Hello, I am ROS Humble, the LTS Version :p ! ";
+  }
+  // message.data = "Hello, I am ROS Humble, the LTS Version :p ! " +
+  // std::to_string(count_++);
+  publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+  tf_static_broadcaster_ =
+      std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
+  this->make_transforms();
+  timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
 
-    auto serviceCallbackPtr = std::bind(&MinimalPublisher::service_callback,
-                                    this,
-                                    std::placeholders::_1,
-                                    std::placeholders::_2);
-    service_ = this->create_service<ros2_beginner_tutorials::
-      srv::ChangeString>(
-      "change_string", serviceCallbackPtr); 
-       }
+  auto serviceCallbackPtr =
+      std::bind(&MinimalPublisher::service_callback, this,
+                std::placeholders::_1, std::placeholders::_2);
+  service_ = this->create_service<ros2_beginner_tutorials::srv::ChangeString>(
+      "change_string", serviceCallbackPtr);
+}
 
 //       MinimalPublisher::tf_static_broadcaster_  =
 //     std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
 
 /**
  * @brief Timer Callback
- * 
+ *
  */
 void MinimalPublisher::timer_callback() {
-
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-      publisher_->publish(message);
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  publisher_->publish(message);
 }
 
 /**
  * @brief Service call
- * 
- * @param request 
- * @param response 
+ *
+ * @param request
+ * @param response
  */
 void MinimalPublisher::service_callback(
-    const std::shared_ptr<ros2_beginner_tutorials::
-    srv::ChangeString::Request> request,
-    std::shared_ptr<ros2_beginner_tutorials::
-    srv::ChangeString::Response> response) {
-    RCLCPP_WARN(rclcpp::get_logger("rclcpp"),
-    "Incoming request\nnew_string: %s", request->update.c_str());
-    message.data = request->update;
-      RCLCPP_DEBUG_STREAM (this->get_logger(), "DEBUG STREAM");
-      RCLCPP_ERROR_STREAM (this->get_logger(), "ERROR CHECK");
-      RCLCPP_FATAL_STREAM (this->get_logger(), "FATAL CHECK");
-    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-    publisher_->publish(message);
-    response->status = "Changed";
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"),
-    "sending back response: [%s]", response->status.c_str()); }
+    const std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Request>
+        request,
+    std::shared_ptr<ros2_beginner_tutorials::srv::ChangeString::Response>
+        response) {
+  RCLCPP_WARN(rclcpp::get_logger("rclcpp"), "Incoming request\nnew_string: %s",
+              request->update.c_str());
+  message.data = request->update;
+  RCLCPP_DEBUG_STREAM(this->get_logger(), "DEBUG STREAM");
+  RCLCPP_ERROR_STREAM(this->get_logger(), "ERROR CHECK");
+  RCLCPP_FATAL_STREAM(this->get_logger(), "FATAL CHECK");
+  RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+  publisher_->publish(message);
+  response->status = "Changed";
+  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "sending back response: [%s]",
+              response->status.c_str());
+}
 
-void MinimalPublisher::make_transforms()
-{
-      geometry_msgs::msg::TransformStamped t;
+void MinimalPublisher::make_transforms() {
+  geometry_msgs::msg::TransformStamped t;
 
-      t.header.stamp = this->get_clock()->now();
-      t.header.frame_id = "world";
-      t.child_frame_id = "talker";
+  t.header.stamp = this->get_clock()->now();
+  t.header.frame_id = "world";
+  t.child_frame_id = "talker";
 
-      t.transform.translation.x = 1;
-      t.transform.translation.y = 0;
-      t.transform.translation.z = 0;
-      tf2::Quaternion q;
-      q.setRPY(0, 0, 90);
-      t.transform.rotation.x = q.x();
-      t.transform.rotation.y = q.y();
-      t.transform.rotation.z = q.z();
-      t.transform.rotation.w = q.w();
+  t.transform.translation.x = 1;
+  t.transform.translation.y = 0;
+  t.transform.translation.z = 0;
+  tf2::Quaternion q;
+  q.setRPY(0, 0, 90);
+  t.transform.rotation.x = q.x();
+  t.transform.rotation.y = q.y();
+  t.transform.rotation.z = q.z();
+  t.transform.rotation.w = q.w();
 
-      RCLCPP_INFO(this->get_logger(), "Broadcasting transformation from world to talker.");
-      RCLCPP_WARN(this->get_logger(), "Broadcasting transformation from world to talker.");
+  RCLCPP_INFO(this->get_logger(),
+              "Broadcasting transformation from world to talker.");
+  RCLCPP_WARN(this->get_logger(),
+              "Broadcasting transformation from world to talker.");
 
-      tf_static_broadcaster_->sendTransform(t);
+  tf_static_broadcaster_->sendTransform(t);
 }
